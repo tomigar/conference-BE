41c7ed75688cd74fed9ea819b845ee383421f5c6
tomigar <tomas.gardlik@gmail.com>
2025-04-01 19:10:12 +0200

    add file upload and show

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,10 @@ use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\ConferenceController;
 use App\Http\Controllers\ConferenceEditorController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\FileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 Route::controller(AuthController::class)->group(function(){
     Route::post('register', 'register');
@@ -13,11 +15,14 @@ Route::controller(AuthController::class)->group(function(){
 });
 
 Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+
     //add routes that are restricted to admin
 });
 
 Route::middleware(['auth:sanctum', 'role:admin,editor'])->group(function () {
     //add routes that are restricted to admin and editor
+    Route::post('/upload', [FileController::class, 'store']);
+    Route::get('/files/{id}', [FileController::class, 'show'])->name('files.show');
 });
 
 Route::middleware('auth:sanctum')->group(function () {
